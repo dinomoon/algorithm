f4e3dca8484d6d67267c57470cc445e210cf943d
MoonK <ansrud45@gmail.com>
2021-06-23 18:33:43 +0900

    0623

--- practice.js
+++ practice.js
@@ -1,38 +1,24 @@
-function solution(numbers) {
-  let answer = 0;
+function solution(record) {
+  const userInfo = {};
+  const action = [];
+  const stateMapping = {
+    Enter: '님이 들어왔습니다.',
+    Leave: '님이 나갔습니다.',
+  };
 
-  // numbers를 배열로 변환하고 내림차순으로 정렬
-  let numbersArr = numbers.split('').sort((a, b) => b - a);
+  record.forEach((v) => {
+    const [state, id, nick] = v.split(' ');
 
-  // 최댓값
-  let N = +numbersArr.join('');
-
-  // 에라토스테네스의 체로 N까지의 숫자 중 소수구하기
-  let primeArr = makePrimeArr(N);
-
-  for (let i = 2; i <= N; i++) {
-    if (primeArr[i] === false) continue;
-    let temp = i.toString().split('');
-    for (let number of numbersArr) {
-      let idx = temp.indexOf(number);
-      if (idx > -1) temp.splice(idx, 1);
+    if (state !== 'Change') {
+      action.push([state, id]);
     }
 
-    if (temp.length === 0) answer++;
-  }
-
-  return answer;
-}
-
-function makePrimeArr(N) {
-  let arr = [];
-  for (let i = 2; i <= N; i++) {
-    if (arr[i] === false) continue;
-    for (let j = i + i; j <= N; j += i) {
-      arr[j] = false;
+    if (nick) {
+      userInfo[id] = nick;
     }
-  }
+  });
 
-  return arr;
+  return action.map(([state, uid]) => {
+    return `${userInfo[uid]}${stateMapping[state]}`;
+  });
 }
-console.log(solution('100'));
