9d346d2ce366bf2f87f6e0c6f502f4f2d9f9f6f2
MoonK <ansrud45@gmail.com>
2021-07-23 17:31:38 +0900

    docs: Review 베스트 앨범

--- practice.js
+++ practice.js
@@ -3,20 +3,5 @@
   let input = require('fs').readFileSync('/dev/stdin').toString();
 */
 
-let input = require('fs').readFileSync('./input.txt').toString().split('\n');
-const N = +input[0];
-
-for (let i = 1; i <= N; i++) {
-  let arr = input[i].split(' ').map(Number);
-  let C = arr[0];
-
-  arr.splice(0, 1);
-  let sum = arr.reduce((a, b) => a + b);
-  let avg = sum / C;
-
-  let count = 0;
-  for (let j = 0; j < C; j++) {
-    if (arr[j] > avg) count++;
-  }
-  console.log(((count / arr.length) * 100).toFixed(3) + '%');
-}
+let input = require('fs').readFileSync('./input.txt').toString();
+console.log(String.fromCharCode(97));
