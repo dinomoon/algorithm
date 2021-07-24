8d47debff81ea723e08471e277f25b1439b491c7
MoonK <ansrud45@gmail.com>
2021-07-24 19:59:20 +0900

    docs: Review 124나라의 숫자

--- practice.js
+++ practice.js
@@ -3,5 +3,15 @@
   let input = require('fs').readFileSync('/dev/stdin').toString();
 */
 
-let input = require('fs').readFileSync('./input.txt').toString();
-console.log(String.fromCharCode(97));
+let input = require('fs').readFileSync('./input.txt').toString().split('\n');
+const N = +input[0];
+for (let i = 1; i <= N; i++) {
+  const split = input[i].split(' ');
+  const repeatN = +split[0];
+  const str = split[1];
+  let answer = '';
+  for (let j = 0; j < str.length; j++) {
+    answer += str[j].repeat(repeatN);
+  }
+  console.log(answer);
+}
