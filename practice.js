01276006956d1db73b2ff6149819c1e95fcaa7e4
MoonK <ansrud45@gmail.com>
2021-08-27 10:13:29 +0900

    docs: Add 강의/삼각형 판별하기

--- practice.js
+++ practice.js
@@ -1,10 +1,15 @@
 function solution(a, b, c) {
-  let min = Number.MAX_SAFE_INTEGER;
-  if (a < min) min = a;
-  if (b < min) min = b;
-  if (c < min) min = c;
+  let answer = 'NO',
+    max;
+  let sum = a + b + c;
 
-  return min;
+  if (a > b) max = a;
+  else max = b;
+  if (c > max) max = c;
+
+  if (sum - max > max) answer = 'YES';
+
+  return answer;
 }
 
-console.log(solution(6, 5, 11));
+console.log(solution(13, 33, 17));
