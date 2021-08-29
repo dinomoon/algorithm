733953bef25eaea8dc7acc6ebd95c0b9407ab749
MoonK <ansrud45@gmail.com>
2021-08-29 18:20:27 +0900

    docs: Add 격자판 최대합, 봉우리

--- practice.js
+++ practice.js
@@ -1,15 +1,33 @@
-// for of
-// map, filter, reduce
-// toUpperCase, toLowerCase
-// substring, substr
-// indexOf
-
 function solution(arr) {
-  let answer;
+  let answer = 0;
+  let n = arr.length;
 
-  answer = arr.filter((v, i) => arr.indexOf(v) === i);
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let flag = 1;
+      for (let k = 0; k < 4; k++) {
+        let nx = dx[k] + i;
+        let ny = dy[k] + j;
+        if (0 <= nx && nx < n && 0 <= ny && ny < n && arr[i][j] < arr[nx][ny]) {
+          flag = 0;
+          break;
+        }
+      }
+      if (flag) answer++;
+    }
+  }
 
   return answer;
 }
 
-console.log(solution(['good', 'time', 'good', 'time', 'student']));
+let arr = [
+  [5, 3, 7, 2, 3],
+  [3, 7, 1, 6, 1],
+  [7, 2, 5, 3, 4],
+  [4, 3, 6, 4, 1],
+  [8, 7, 3, 5, 2],
+];
+console.log(solution(arr));
