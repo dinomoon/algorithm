bb568abf01d5e84fec6f3c9f45892fd549e4c3ff
MoonK <ansrud45@gmail.com>
2021-09-02 18:29:39 +0900

    docs: Add 올바른 괄호

--- practice.js
+++ practice.js
@@ -1,34 +1,25 @@
-function solution(a, b) {
+function solution(a) {
   let answer = 'YES';
-  let aMap = new Map();
-  let bMap = new Map();
+
+  let stack = [];
 
   for (let x of a) {
-    if (aMap.has(x)) {
-      aMap.set(x, aMap.get(x) + 1);
+    if (x === '(') {
+      stack.push(x);
     } else {
-      aMap.set(x, 1);
+      if (stack.length === 0) {
+        return 'NO';
+      }
+      stack.pop();
     }
   }
 
-  for (let x of b) {
-    if (bMap.has(x)) {
-      bMap.set(x, bMap.get(x) + 1);
-    } else {
-      bMap.set(x, 1);
-    }
-  }
-
-  for (let [key, value] of aMap) {
-    if (value !== bMap.get(key)) {
-      answer = 'NO';
-      break;
-    }
+  if (stack.length !== 0) {
+    return 'NO';
   }
 
   return answer;
 }
 
-let a = 'AbaAeCea';
-let b = 'baeeACA';
-console.log(solution(a, b));
+let a = '()';
+console.log(solution(a));
