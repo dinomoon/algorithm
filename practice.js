632254e0f6ebd107fe23d2fd60f669bb7078647e
MoonK <ansrud45@gmail.com>
2021-09-09 16:01:22 +0900

    docs: Add 결혼식

--- practice.js
+++ practice.js
@@ -1,8 +1,29 @@
-function solution(n, arr) {
-  let answer = Array.from({ length: n }, () => 0);
+function solution(times) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let T_line = [];
+  for (let x of times) {
+    T_line.push([x[0], 's']);
+    T_line.push([x[1], 'e']);
+  }
+  T_line.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+    else return a[0] - b[0];
+  });
 
+  let cnt = 0;
+  for (let x of T_line) {
+    if (x[1] === 's') cnt++;
+    else cnt--;
+    answer = Math.max(answer, cnt);
+  }
   return answer;
 }
 
-let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
-console.log(solution(5, arr));
+let arr = [
+  [14, 18],
+  [12, 15],
+  [15, 20],
+  [20, 30],
+  [5, 14],
+];
+console.log(solution(arr));
