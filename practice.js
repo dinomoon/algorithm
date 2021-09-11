b641b724c8af348d1708cfe2c996b41c1b545d41
MoonK <ansrud45@gmail.com>
2021-09-11 13:43:35 +0900

    docs: Update 가장 짧은 문자거리

--- practice.js
+++ practice.js
@@ -1,35 +1,28 @@
-function count(songs, capacity) {
-  let cnt = 1;
-  let sum = 0;
-  for (let x of songs) {
-    if (sum + x <= capacity) {
-      sum += x;
+function solution(str, s) {
+  let answer = [];
+  let p = 1000;
+
+  for (let x of str) {
+    if (x === s) {
+      p = 0;
+      answer.push(p);
     } else {
-      cnt++;
-      sum = x;
+      p++;
+      answer.push(p);
     }
   }
 
-  return cnt;
-}
-
-function solution(m, songs) {
-  let answer;
-  let lt = Math.min(...songs);
-  let rt = songs.reduce((a, b) => a + b);
-
-  while (lt <= rt) {
-    let mid = parseInt((lt + rt) / 2);
-    if (count(songs, mid) <= m) {
-      answer = mid;
-      rt = mid - 1;
+  p = 1000;
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] === s) {
+      p = 0;
     } else {
-      lt = mid + 1;
+      p++;
+      answer[i] = Math.min(answer[i], p);
     }
   }
 
   return answer;
 }
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(solution(3, arr));
+let str = 'teachermode';
+console.log(solution(str, 'e'));
