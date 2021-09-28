23304b8b2a9794455d2163aeb93af925e6647e28
MoonK <ansrud45@gmail.com>
2021-09-28 17:34:20 +0900

    docs: Add 등수 구하기

--- practice.js
+++ practice.js
@@ -1,24 +1,17 @@
 function solution(arr) {
-  let answer = 'NO';
-  let flag = 0;
-  let arrSum = arr.reduce((a, b) => a + b, 0);
+  let n = arr.length;
+  answer = Array.from({ length: n }, () => 1);
 
-  function DFS(L, sum) {
-    if (flag === 1) return;
-    if (L === arr.length) {
-      if (arrSum - sum === sum) {
-        answer = 'YES';
-        flag = 1;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[j] > arr[i]) {
+        answer[i]++;
       }
-    } else {
-      DFS(L + 1, sum + arr[L]);
-      DFS(L + 1, sum);
     }
   }
 
-  DFS(0, 0);
   return answer;
 }
 
-let arr = [1, 3, 5, 6, 7, 10];
+let arr = [87, 89, 92, 100, 76];
 console.log(solution(arr));
