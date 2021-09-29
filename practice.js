04dfd00b3e9289d017cbe3b1234204a9a5412863
MoonK <ansrud45@gmail.com>
2021-09-29 22:18:17 +0900

    docs: Add 공통원소 구하기, 연속 부분수열2, 학급 회장

--- practice.js
+++ practice.js
@@ -1,29 +1,56 @@
-function solution(a, b) {
-  let answer = [];
-  let m = a.length;
-  let n = b.length;
-  let p1 = 0;
-  let p2 = 0;
+function solution(str1, str2) {
+  let answer = 0;
+  let map1 = new Map();
+  let map2 = new Map();
 
-  while (p1 < m && p2 < n) {
-    if (a[p1] <= b[p2]) {
-      answer.push(a[p1++]);
+  // map2
+  for (let x of str2) {
+    if (map2.has(x)) {
+      map2.set(x, map2.get(x) + 1);
     } else {
-      answer.push(b[p2++]);
+      map2.set(x, 1);
     }
   }
 
-  while (p1 < m) {
-    answer.push(a[p1++]);
+  // map1
+  for (let i = 0; i < str2.length; i++) {
+    let char = str1[i];
+    if (map1.has(char)) {
+      map1.set(char, map1.get(char) + 1);
+    } else {
+      map1.set(char, 1);
+    }
   }
 
-  while (p2 < n) {
-    answer.push(b[p2++]);
+  let lt = 0;
+  let rt = str2.length;
+  while (rt < str1.length) {
+    if (isSame(map1, map2)) {
+      answer++;
+    } else {
+      map1.set(str1[lt], map1.get(str1[lt]) - 1);
+      if (map1.get(str1[lt]) === 0) {
+        map1.delete(str1[lt]);
+      }
+      lt++;
+      if (map1.has(str1[rt])) {
+        map1.set(str1[rt], map1.get(str1[rt]) + 1);
+      } else {
+        map1.set(str1[rt], 1);
+      }
+    }
   }
 
   return answer;
 }
 
-let a = [1, 3, 5];
-let b = [2, 3, 6, 7, 9];
+function isSame(m1, m2) {
+  if (m1.size() !== m2.size()) {
+    return false;
+  } else {
+  }
+}
+
+let a = 'bacaAacba';
+let b = 'abc';
 console.log(solution(a, b));
